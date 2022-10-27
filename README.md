f964da5aaf42b9dbd7af160a06569300e0b8b77e
0xcyberpj <cyberpj404@gmail.com>
2022-10-27 22:12:48 +0530

    0x0

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# c0vertX
-c0vertX - The Art of Hiding Messages in Plain Sight 
+<center>
+<h1>C0VERTX &nbsp;<sub> <sub>- P4ul Jeremi4h</sub>
+</h1>
+</center>
+
+<center>
+
+![](finallogo.png) 
+</center>
+
+
+<h2>An Art Of Plain Sight <b>Steganography !</b> </h2> <hr>
+
+ <b>Steganography Tool &nbsp;</b><sub>- P4ul Jeremi4h</sub>
+<br>
+
+</
+center>
+
+```
+[p4ul@Earth~]$ cat Author.txt
+
+P4ul Jeremi4h
+```
+</center>
+
+<hr>
+<br>
+
+## ❯ Usage :
+<br>
+
+## `Encoding : `
+<br>
+
+```bash
+python3 c0vertX.py -e <secret text>  <encodedoutput_filename> 
+
+```
+*Example :*
+
+<center>
+
+```
+❯ python3 c0vertX.py -e 'The-super-secret-forever' secret.txt
+
+file saved as : secret.txt
+```
+</center>
+
+**content of** &nbsp; ` secret.txt` :
+
+```bash
+❯ head secret.txt
+
+
+
+
+
+
+
+
+
+```
+
+## `Decoding : `
+
+<center>
+
+```
+python3  c0vertX.py -d <encoded-text-file>
+```
+
+</center>
+
+**Example :**
+
+```
+❯ python3 c0vertX.py -d secret.txt
+
+The-super-secret-forever
+
+❯ cat  decoded.txt
+
+The-super-secret-forever
+```
+
+<br>
+
+# [+] About  : 
+
+```
+0.Command Line STEGANOGRAPHY TOOL Written in PYTHON 
+
+1. Command Line support 
+
+2.Easy to Automate
+
+3.Both Encode and Decode Option
+
+4.'\u200b' char is used to encode the text
+
+```
---
+++ c0vertX.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+#Author = P4ul 
+#date : oct 3 2022
+
+import sys
+import os
+
+
+
+def enc():
+    text=sys.argv[2]
+    nameoffile=sys.argv[3]
+
+    word_ord=[]
+
+    secret_null=[]
+    file=open(f'{nameoffile}',"w")
+    for i in range(len(text)):
+	    word_ord.append(ord(text[i]))
+
+
+
+    for j in word_ord:
+	    secret_null.append("\u200b"*j)
+
+    for i in range(len(secret_null)):
+	    file.write(secret_null[i])
+	    file.write("\n")
+    file.close()
+    print('\n\t\t\033[1;32m C - 0 - V - E - R  - T - X \n')
+    print("\t\t\033[1;3m  File Saved as : "+str(file.name))
+
+
+
+def decoder():
+    sec=''
+    reader=sys.argv[2]
+    encfile=open(reader,'rb')
+    num=len(encfile.readlines())
+    output=open('decoded.txt','w')
+    anotherfile=open(reader,'r')
+    for i in range(num):
+        sec+=chr(len(anotherfile.readline())-1)
+    output.write(sec)
+    print(sec)
+    output.close()
+    anotherfile.close()
+    encfile.close()
+
+
+try:
+	if((sys.argv[1]=='-d') and (len(sys.argv)==3)):
+		decoder()
+	elif ((sys.argv[1]=='-e') and (len(sys.argv)==4)):
+		enc()
+	else:
+		print("Invalid BRO")
+except Exception as e:
+	print("Invalid Input")
+	print('''
+Encoding : ./c0vertX.py -e <secret text> <encoded output_filename> 
+
+Decoding : ./c0vertX.py -d <encoded-text>
+''')
+
+
+
+
